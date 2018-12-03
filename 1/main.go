4be2611ee013ae87fae6454f67d6d92c41c28ba1
Ilan Pillemer <ilan.pillemer@gmail.com>
2018-12-03 00:01:14 +0000

    day 1 part 2

--- 1/main.go
+++ 1/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 	"strings"
@@ -17,23 +18,54 @@ func (f *freq) apply(input int) {
 	f.value = f.value + input
 }
 
+var calibrating = flag.Bool("c", false, "calibrating mode")
+
 func main() {
 	f := freq{0}
 	r := bufio.NewReader(os.Stdin)
 
 	flag.Parse()
-	switch flag.Arg(0) {
-
-// this solves part 1
-	default:
+	if *calibrating {
+		fmt.Println("calibrating")
+		a := turnIntoArray(r)
+		seen := make(map[int]int)
+		seen[0] = seen[0] + 1
+		i := 0
 		for {
-			input, _ := r.ReadString('\n')
-			input = strings.TrimSpace(input)
-			if strings.TrimSpace(input) == "" {
+			count := seen[f.value]
+			if count == 2 {
 				fmt.Println(f.value)
 				os.Exit(0)
 			}
-			f.apply(nice(input))
+			f.apply(a[i])
+			seen[f.value] = seen[f.value] + 1
+			i = (i + 1) % len(a)
+		}
+	}
+
+	// not calibrating, so part 1
+	for {
+		input, err := r.ReadString('\n')
+		input = strings.TrimSpace(input)
+		if strings.TrimSpace(input) == "" && err == io.EOF {
+			fmt.Println(f.value)
+			os.Exit(0)
+		}
+		f.apply(nice(input))
+	}
+
+}
+
+func turnIntoArray(r *bufio.Reader) []int {
+	a := make([]int, 0)
+	for {
+		input, err := r.ReadString('\n')
+		input = strings.TrimSpace(input)
+		if strings.TrimSpace(input) == "" && err == io.EOF {
+			return a
+		}
+		if strings.TrimSpace(input) != "" {
+			a = append(a, nice(input))
 		}
 	}
 }
@@ -42,6 +74,9 @@ func nice(s string) int {
 	i, err := strconv.Atoi(s)
 	if err != nil {
 		panic(err.Error())
+	}
+	if i == 0 {
+		fmt.Println("wtf")
 	}
 	return i
 }
