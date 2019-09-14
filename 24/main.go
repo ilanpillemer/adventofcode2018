4ad52487ff2883f83d7858fff02d947cad95f3b0
Ilan Pillemer <ilan.pillemer@gmail.com>
2019-09-14 21:01:21 +0100

    more

--- 24/main.go
+++ 24/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type attackType int
 
@@ -12,27 +15,58 @@ const (
 )
 
 type unit struct {
-	hp     int
-	damage int
-	attack attackType
-	immune []attackType
-	weak   []attackType
+	hp int
+}
+
+type group struct {
+	units      []unit
+	damage     int
+	initiative int
+	attack     attackType
+	immune     []attackType
+	weak       []attackType
+}
+
+func (a army) Len() int {
+	return len(a.groups)
+}
+
+func (a army) Less(i, j int) bool {
+	if a.groups[i].power() == a.groups[j].power() {
+		return a.groups[i].initiative < a.groups[j].initiative
+	}
+
+	return a.groups[i].power() < a.groups[j].power()
+}
+
+// Swap swaps the elements with indexes i and j.
+func (a army) Swap(i, j int) {
+	a.groups[i], a.groups[j] = a.groups[j], a.groups[i]
 }
 
 type army struct {
-	group []unit
+	groups []group
 }
 
-func power(group []unit) int {
-	if len(group) == 0 {
+func (g group) power() int {
+	if len(g.units) == 0 {
 		return 0
 	}
 
-	return len(group) * group[0].damage
+	return len(g.units) * g.damage
 }
 
 func main() {
 	is := army{}
 	team := army{}
 	fmt.Println(is, team)
+}
+
+func (a *army) target(t *army) {
+	sort.Sort(a)
+	fmt.Println(a)
+	//	for i, v := range a.group {
+	//
+	//	}
+
 }
--- 24/main_test.go
+++ 24/main_test.go
@@ -1,16 +1,61 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"sort"
+	"testing"
+)
 
 func TestPower(t *testing.T) {
-	group := make([]unit, 18)
+	g := group{}
+	g.units = make([]unit, 18)
 	for i := 0; i < 18; i++ {
-		group[i] = unit{damage: 8}
+		g.damage = 8
+		g.units[i] = unit{}
 	}
 
 	want := 144
-	got := power(group)
+	got := g.power()
 	if want != got {
 		t.Errorf("wanted %d got %d", want, got)
 	}
+}
+
+func TestSort(t *testing.T) {
+	a := army{}
+	g0 := group{}
+	g0.units = make([]unit, 18)
+	for i := 0; i < 18; i++ {
+		g0.damage = 1
+		g0.units[i] = unit{}
+	}
+
+	g1 := group{}
+	g1.units = make([]unit, 1)
+	for i := 0; i < 1; i++ {
+		g1.damage = 8
+		g1.units[i] = unit{}
+	}
+
+	g2 := group{}
+	g2.units = make([]unit, 180)
+	for i := 0; i < 180; i++ {
+		g2.damage = 8
+		g2.units[i] = unit{}
+	}
+
+	a.groups = []group{g0, g1, g2}
+	sort.Sort(a)
+	if a.groups[0].power() != 8 {
+		t.Errorf("want 8 got %d", a.groups[0].power())
+	}
+
+	if a.groups[1].power() != 18 {
+		t.Errorf("want 18 got %d", a.groups[1].power())
+	}
+
+	if a.groups[2].power() != 1440 {
+		t.Errorf("want 1440 got %d", a.groups[2].power())
+	}
+
 }
